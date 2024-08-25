0a4b5dde2cb02ac23034e520fca26fe4401b8455
buinhathuy511 <huynhatbui123@gmail.com>
2024-08-25 19:21:54 +0700

    fix: routerMethod

--- routers/methods.js
+++ routers/methods.js
@@ -2,22 +2,22 @@ const { httpMethods } = require('../constants');
 
 const routerMethods = {
     get: function(request, response, path, callback) {
-        if (request.method === httpMethods.GET && request.url.startsWith(path)) {
+        if (request.method === httpMethods.GET && request.url === path) {
             callback(request, response);
         }
     },
     post: function(request, response, path, callback) {
-        if (request.method === httpMethods.POST && request.url.startsWith(path)) {
+        if (request.method === httpMethods.POST && request.url === path) {
             callback(request, response);
         }
     },
     put: function(request, response, path, callback) {
-        if (request.method === httpMethods.PUT && request.url.startsWith(path)) {
+        if (request.method === httpMethods.PUT && request.url === path) {
             callback(request, response);
         }
     },
     delete: function(request, response, path, callback) {
-        if (request.method === httpMethods.DELETE && request.url.startsWith(path)) {
+        if (request.method === httpMethods.DELETE && request.url === path) {
             callback(request, response);
         }
     }
